815f61fee332612b15df16780764cc092699dc84
Halloloid <animation12321567@gmail.com>
2026-03-17 20:11:11 +0530

    url corrected

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://task-managment-vert.vercel.app/",
+    origin: "https://task-managment-vert.vercel.app",
     credentials: true
 }))
 app.use(cookieParser())
