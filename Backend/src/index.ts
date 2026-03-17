779106bb542b861ad114f16e9bce111688e3e2ab
Halloloid <animation12321567@gmail.com>
2026-03-17 20:05:35 +0530

    Added Frontend Url

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-managment-vert.vercel.app/",
     credentials: true
 }))
 app.use(cookieParser())
